3203d0997f8cefaa894906ede36bdf8dce42b478
shivamshekhar <shivam.shekhar.ece14@itbhu.ac.in>
2017-03-14 22:58:42 +0530

    defragmented the main file

---
+++ davis.py
@@ -0,0 +1,107 @@
+import pygame
+from constants import *
+from pygame import *
+from functions import *
+
+class Davis(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        self.images,self.rect = load_sprite_sheet('davis_0.bmp',10,7,70,70,-1)
+        self.images1,self.rect = load_sprite_sheet('davis_1.bmp',10,7,70,70,-1)
+        self.images2,self.rect = load_sprite_sheet('davis_2.bmp',10,7,70,70,-1)
+        self.image = self.images[0]
+        self.index = 0
+
+        self.isWalking = False
+        self.walk_arr = [4,5,6,7,6,5]
+        self.walk_arr_index = 0
+        self.walk_blit_rate = 3
+        self.walk_speed = 5
+        self.direction = 1
+
+        self.stand_arr = [0,1,2,3,2,1]
+        self.stand_arr_index = 0
+        self.stand_blit_rate = 4
+
+        self.isPunching = False
+        self.punch_arr = [10,11,12,13,14,15,16,17]
+        self.punch_arr_index = 0
+        self.punch_blit_rate = 2
+
+        self.isFiringBlast = False
+        self.firingBlast_arr = [0,1,2,3,4,5,6,7,8,9]
+        self.firingBlast_arr_index = 0
+        self.firingBlast_blit_rate = 2
+
+        self.movement = [0,0]
+
+        self.rect.left = x
+        self.rect.top = y
+
+        self.sheet_number = 0
+
+        self.counter = 0
+
+    def draw(self):
+        screen.blit(self.image,self.rect)
+
+    def update(self):
+        if self.isWalking:
+            if self.counter%self.walk_blit_rate == 0:
+                self.index = self.walk_arr[self.walk_arr_index]
+                self.walk_arr_index = (self.walk_arr_index + 1)%len(self.walk_arr)
+
+            self.rect = self.rect.move(self.movement)
+            self.sheet_number = 0
+
+
+        elif self.isPunching:
+            if self.counter%self.punch_blit_rate == 0:
+                self.index = self.punch_arr[self.punch_arr_index]
+                self.punch_arr_index = (self.punch_arr_index + 1)%len(self.punch_arr)
+            self.sheet_number = 0
+
+        elif self.isFiringBlast:
+            if self.counter%self.firingBlast_blit_rate == 0:
+                self.index = self.firingBlast_arr[self.firingBlast_arr_index]
+                self.firingBlast_arr_index = (self.firingBlast_arr_index + 1)%len(self.firingBlast_arr)
+            self.sheet_number = 2
+
+        else:
+            if self.counter%self.stand_blit_rate == 0:
+                self.index = self.stand_arr[self.stand_arr_index]
+                self.stand_arr_index = (self.stand_arr_index + 1)%len(self.stand_arr)
+            self.sheet_number = 0
+
+        if self.sheet_number == 0:
+            self.image = self.images[self.index]
+        elif self.sheet_number == 1:
+            self.image = self.images1[self.index]
+        elif self.sheet_number == 2:
+            self.image = self.images2[self.index]
+
+        if self.direction < 0:
+            self.image = pygame.transform.flip(self.image,True,False)
+
+        self.counter = (self.counter + 1)%10000000000
+        self.checkbounds()
+
+    def checkbounds(self):
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > width:
+            self.rect.right = width
+        if self.rect.top < int(height*0.25):
+            self.rect.top = int(height*0.25)
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+
+class DavisBall(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self,self.containers)
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
---
+++ functions.py
@@ -0,0 +1,64 @@
+import os
+import pygame
+from pygame import *
+from constants import *
+
+def load_image(
+    name,
+    sizex=-1,
+    sizey=-1,
+    colorkey=None,
+    ):
+
+    fullname = os.path.join('sprites', name)
+    image = pygame.image.load(fullname)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+
+    if sizex != -1 or sizey != -1:
+        image = pygame.transform.scale(image, (sizex, sizey))
+
+    return (image, image.get_rect())
+
+def load_sprite_sheet(
+        sheetname,
+        nx,
+        ny,
+        scalex = -1,
+        scaley = -1,
+        colorkey = None,
+        ):
+    fullname = os.path.join('sprites',sheetname)
+    sheet = pygame.image.load(fullname)
+    sheet = sheet.convert()
+
+    sheet_rect = sheet.get_rect()
+
+    sprites = []
+
+    sizex = sheet_rect.width/nx
+    sizey = sheet_rect.height/ny
+
+    for i in range(0,ny):
+        for j in range(0,nx):
+            rect = pygame.Rect((j*sizex,i*sizey,sizex,sizey))
+            image = pygame.Surface(rect.size)
+            image = image.convert()
+            image.blit(sheet,(0,0),rect)
+
+            if colorkey is not None:
+                if colorkey is -1:
+                    colorkey = image.get_at((0,0))
+                image.set_colorkey(colorkey,RLEACCEL)
+
+            if scalex != -1 or scaley != -1:
+                image = pygame.transform.scale(image,(scalex,scaley))
+
+            sprites.append(image)
+
+    sprite_rect = sprites[0].get_rect()
+
+    return sprites,sprite_rect
